4dd051c840a37daab2ebf3bb1c5a55e4fe8cfa7c
Rafael Oliveira <rafael.oliveira@modalgr.com.br>
2023-05-26 17:29:19 -0300

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<S-s>", ":HopWord<cr>", { silent = true, desc = "HopWord" })
+vim.keymap.set("n", "<leader>cw", ":Telescope diagnostics<cr>", { silent = true, desc = "Diagnostics" })
