b3d6547591e73fd18749ece25507644e1929d5b5
Rafael Oliveira <rafael.oliveira@modalgr.com.br>
2023-05-27 19:28:14 -0300

    add snippets support

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ require("config.lazy")
 require("hop").setup()
 require("nvim-ts-autotag").setup()
 require("mini.splitjoin").setup()
+
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
---
+++ lua/snippets/typescriptreact.lua
@@ -0,0 +1,53 @@
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  s("trig1", t("Typescript component loaded!!")),
+  s(
+    "tscomponent",
+    fmt(
+      [[
+  import React from 'react'
+  
+  const {component_name}: React.FC = () => {{
+    return (
+      <h1>{component_name}</h1>
+    )
+  }}
+
+  export default {component_name}
+  ]],
+      {
+        component_name = i(1, "component_name"),
+      },
+      {
+        repeat_duplicates = true,
+      }
+    )
+  ),
+  s(
+    "tsicomponent",
+    fmt(
+      [[
+  import React from 'react'
+
+  interface I{component_name}Props {{
+    prop?: any 
+  }}
+
+  const {component_name}: React.FC<I{component_name}Props> = () => {{
+    return (
+      <h1>{component_name}</h1>
+    )
+  }}
+
+  export default {component_name}
+  ]],
+      {
+        component_name = i(1, "component_name"),
+      },
+      {
+        repeat_duplicates = true,
+      }
+    )
+  ),
+}
