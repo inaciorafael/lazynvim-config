2ccccbf2b2612a7b1894ba0a493195a0fbb61dd3
Rafael Oliveira <rafael.oliveira@modalgr.com.br>
2023-06-10 18:00:01 -0300

    CRON commit

--- init.lua
+++ init.lua
@@ -10,4 +10,8 @@ require("NeoComposer").setup()
 -- Load snippets
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
 
-vim.fn.setreg("a", "itest")
+vim.fn.setreg("a", "itest^[0v$yp")
+vim.fn.setreg("i", "0vlyiimport <esc>ebvegza{<esc>A from ''<esc>ireact-icons/<esc>pbveu0")
+
+-- RxDashboard
+-- RxDashboard
